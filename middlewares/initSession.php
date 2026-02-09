6e185676ba4be8d04e41fe0d219a04b0d1800951
gatien <gatien.parat@hotmail.fr>
2026-02-09 16:57:41 +0100

    commentaire supprimé

--- middlewares/initSession.php
+++ middlewares/initSession.php
@@ -1,11 +1,10 @@
 <?php
 
-/*A enlever en production */
 $isHttps = (
     (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
     || $_SERVER['SERVER_PORT'] == 443
 );
-/*FIN A enlever en production */
+
 
 
 session_set_cookie_params([
